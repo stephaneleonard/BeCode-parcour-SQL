5f6fbf7c204b70586fc67e2862f1593be82232ea
stephaneleonard <stefleonard97@gmail.com>
2020-03-15 13:04:03 +0100

    revert error

--- weatherApp/db.sql
+++ weatherApp/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: Mar 12, 2020 at 01:46 PM
+-- Generation Time: Mar 15, 2020 at 11:58 AM
 -- Server version: 8.0.19
 -- PHP Version: 7.4.1
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `weatherApp`
 --
+CREATE DATABASE IF NOT EXISTS `weatherApp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `weatherApp`;
 
 -- --------------------------------------------------------
 
@@ -29,7 +31,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `Météo` (
-  `ville` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ville` varchar(9) DEFAULT NULL,
   `haut` int DEFAULT NULL,
   `bas` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -42,7 +44,8 @@ INSERT INTO `Météo` (`ville`, `haut`, `bas`) VALUES
 ('Bruxelles', 27, 13),
 ('Liège', 25, 15),
 ('Namur', 26, 15),
-('Charleroi', 25, 12);
+('Charleroi', 25, 12),
+('Bruges', 28, 16);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
